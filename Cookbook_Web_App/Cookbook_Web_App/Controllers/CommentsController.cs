764c2472170743fa774f7bb2cc7d749abed372bb
ntibbals <ntibbals@gmail.com>
2019-02-04 09:02:20 -0800

    added views

---
+++ Cookbook_Web_App/Cookbook_Web_App/Controllers/CommentsController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook_Web_App.Controllers
+{
+    public class CommentsController
+    {
+    }
+}
