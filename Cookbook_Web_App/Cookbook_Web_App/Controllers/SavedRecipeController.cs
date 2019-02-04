810c78bb91bd79ab8e9d3ec9e5a09fede2408bc5
Tulip Pink <pinktulips@hotmail.com>
2019-02-04 11:07:46 -0800

    delete saved recipe

--- Cookbook_Web_App/Cookbook_Web_App/Controllers/SavedRecipeController.cs
+++ Cookbook_Web_App/Cookbook_Web_App/Controllers/SavedRecipeController.cs
@@ -55,5 +55,83 @@ namespace Cookbook_Web_App.Controllers
             return View(savedRecipe);
         }
 
+        //Edit SavedRecipe
+        public async IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var savedRecipe = await _context.SavedRecipe.FirstOrDefault(s => s.ID == id);
+            if (savedRecipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(SavedRecipe);
+        }
+
+        //Post: Edit SavedRecipe
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,APIReference")] SavedRecipe savedRecipe)
+        {
+            if (id != savedRecipe.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(savedRecipe);
+                    await _context.SaveChangesAsync();
+
+                }
+                catch (Exception)
+                {
+                    if (!SavedRecipeExists(savedRecipe.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+
+                }
+            }
+            return View(savedRecipe);
+        }
+
+        //Get Delete SavedRecipe
+        public async Task<IActionResult> Delete(int id)
+        {
+            var savedRecipe = _context.SavedRecipe.FirstOrDefault(s => s.ID == id);
+            if (savedRecipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(SavedRecipe);
+        }
+
+        //Delete SavedRecipe
+        [HttpPost]
+        public async Task<IActionResult> ConfirmDelete(int id)
+        {
+            var savedRecipe = _context.SavedRecipe.FirstOrDefault(s => s.ID == id);
+            _context.SavedRecipe.Remove(savedRecipe);
+
+            await _context.SaveChangesAsync();
+            return View(); 
+        }
+
+
+        private bool SavedRecipeExists(int id)
+        {
+            return _context.SavedRecipe.Any(s => s.ID == id);
+        }
     }
 }
