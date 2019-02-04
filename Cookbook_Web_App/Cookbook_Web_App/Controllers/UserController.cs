21f967f659eba15674018216e63899a0a9e74b23
Clarice Costello <claricostello@outlook.com>
2019-02-04 09:23:10 -0800

    set up context for UserController

---
+++ Cookbook_Web_App/Cookbook_Web_App/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Cookbook_Web_App.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook_Web_App.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly CookbookDbContext _context;
+
+        public UserController(CookbookDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
