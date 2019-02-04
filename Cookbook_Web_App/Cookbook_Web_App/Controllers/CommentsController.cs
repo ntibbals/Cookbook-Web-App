3d8fa1c72144f1492efee5864de6041108745891
ntibbals <ntibbals@gmail.com>
2019-02-04 10:25:40 -0800

    setup get comments method

--- Cookbook_Web_App/Cookbook_Web_App/Controllers/CommentsController.cs
+++ Cookbook_Web_App/Cookbook_Web_App/Controllers/CommentsController.cs
@@ -1,11 +1,41 @@
-﻿using System;
+﻿using Cookbook_Web_App.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Cookbook_Web_App.Controllers
 {
-    public class CommentsController
+    public class CommentsController : Controller
     {
+        private readonly CookbookDbContext _context;
+
+        public CommentsController(CookbookDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get Comments
+        /// </summary>
+        /// <param name="ID">comment ID</param>
+        /// <returns>Comments</returns>
+        public async Task<IActionResult> IndexAsync(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var comment =  await _context.Comments.FirstOrDefaultAsync(co => co.ID == id);
+            if (comment ==null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
     }
 }
