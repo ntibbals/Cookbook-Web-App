2a5f3a509ef9de174234634ca3d96b556ad7e0b1
Tulip Pink <pinktulips@hotmail.com>
2019-02-04 10:09:38 -0800

    Saved Recipe Controller

---
+++ Cookbook_Web_App/Cookbook_Web_App/Controllers/SavedRecipeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook_Web_App.Controllers
+{
+    public class SavedRecipeController : Controller  
+    {
+    }
+}
