ec935eaafb1dfa5554da9617c9c0d6f2f25bd81c
Tulip Pink <pinktulips@hotmail.com>
2019-02-04 10:55:12 -0800

    troubleshooting saved recipe controller

--- Cookbook_Web_App/Cookbook_Web_App/Controllers/SavedRecipeController.cs
+++ Cookbook_Web_App/Cookbook_Web_App/Controllers/SavedRecipeController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cookbook_Web_App.Data;
+using Cookbook_Web_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Cookbook_Web_App.Data;
-using Cookbook_Web_App.Models;
 
 namespace Cookbook_Web_App.Controllers
 {
@@ -18,15 +19,41 @@ namespace Cookbook_Web_App.Controllers
         }
 
         //Get SavedRecipe
-        public IActionResult Index(int? ID)
+        public async Task<IActionResult> Index(int? ID)
         {
             if (ID == null)
             {
                 return NotFound();
             }
-            return View(savedRecipe);
+
+            var user = await _context.SavedRecipe.FirstOrDefault(s => s.ID == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);          
         }
 
         //Create SavedRecipe
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        public async IActionResult Create([Bind("ID,APIReference")] SavedRecipe savedRecipe)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(savedRecipe);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(savedRecipe);
+        }
+
     }
 }
