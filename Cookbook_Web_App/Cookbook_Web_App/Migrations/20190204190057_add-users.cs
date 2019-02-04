26128adcb33ab0f5338f8f91285f29368aea04ee
ntibbals <ntibbals@gmail.com>
2019-02-04 11:44:56 -0800

    working on comments views

---
+++ Cookbook_Web_App/Cookbook_Web_App/Migrations/20190204190057_add-users.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cookbook_Web_App.Migrations
+{
+    public partial class addusers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
