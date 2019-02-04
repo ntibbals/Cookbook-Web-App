2da5846e486b6edf5e6ebf5b2949c52e56ce0b62
ntibbals <ntibbals@gmail.com>
2019-02-04 09:43:10 -0800

    setup db context and startup

--- Cookbook_Web_App/Cookbook_Web_App/Data/CookbookDbContext.cs
+++ Cookbook_Web_App/Cookbook_Web_App/Data/CookbookDbContext.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using Cookbook_Web_App.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Cookbook_Web_App.Data
 {
-    public class CookbookDbContext
+    public class CookbookDbContext : DbContext
     {
+
+        public CookbookDbContext(DbContextOptions<CookbookDbContext> options) : base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// While creating model, able to set key creations, informaiton you need to know about while db is being constructed
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Comments> Comments { get; set; }
     }
 }
