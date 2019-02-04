b7b9716f6d766d6851afb63206d9e32651b22d8b
Tulip Pink <pinktulips@hotmail.com>
2019-02-04 10:19:05 -0800

    Adding to saved recipe controller

--- Cookbook_Web_App/Cookbook_Web_App/Controllers/SavedRecipeController.cs
+++ Cookbook_Web_App/Cookbook_Web_App/Controllers/SavedRecipeController.cs
@@ -3,10 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cookbook_Web_App.Data;
+using Cookbook_Web_App.Models;
 
 namespace Cookbook_Web_App.Controllers
 {
     public class SavedRecipeController : Controller  
     {
+        private readonly CookbookDbContext _context;
+
+        public SavedRecipeController(CookbookDbContext context)
+        {
+            _context = context;
+        }
+
     }
 }
