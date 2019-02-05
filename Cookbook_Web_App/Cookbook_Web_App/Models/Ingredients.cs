0b167a1d0b1992f8815dc398f839cddd7964ae4e
Clarice Costello <claricostello@outlook.com>
2019-02-05 15:33:49 -0800

    built out models and api connection

--- Cookbook_Web_App/Cookbook_Web_App/Models/Ingredients.cs
+++ Cookbook_Web_App/Cookbook_Web_App/Models/Ingredients.cs
@@ -1,16 +1,15 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Cookbook_Web_App.Models
 {
-    public class Recipe
+    public class Ingredients
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public IEnumerable<Instructions> Instructions { get; set; }
-        public IEnumerable<Ingredients> Ingredients { get; set; }
+        public string Quantity { get; set; }
+        public int RecipeID { get; set; }
     }
 }
---
+++ Cookbook_Web_App/Cookbook_Web_App/Models/RecipeIngredients.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook_Web_App.Models
+{
+    public class RecipeIngredients
+    {
+        public int recipeID { get; set; }
+        public int ingredientsID { get; set; }
+        public string quantity { get; set; }
+    }
+}
