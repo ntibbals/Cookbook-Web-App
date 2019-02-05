357aa252da9e2cbb150a78fedf6b1551249a535b
Clarice Costello <claricostello@outlook.com>
2019-02-05 13:30:56 -0800

    Added UserID to SavedRecipe table, added filter to Index method based on userID

--- Cookbook_Web_App/Cookbook_Web_App/Models/SavedRecipem.cs
+++ Cookbook_Web_App/Cookbook_Web_App/Models/SavedRecipem.cs
@@ -7,7 +7,8 @@ namespace Cookbook_Web_App.Models
 {
     public class SavedRecipe
     {
-        public int SavedRecipeID { get; set; }
+        public int SavedRecipeID { get; set; }        
+        public int UserID { get; set; }
         public int APIReference { get; set; }
         public string Name { get; set; }
 
