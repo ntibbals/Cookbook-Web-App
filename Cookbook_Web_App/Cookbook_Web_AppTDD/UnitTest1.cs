23e010146c5a7b7e038278660cac588b545a558a
Tulip Pink <pinktulips@hotmail.com>
2019-02-06 13:56:47 -0800

    testing getters and setters

---
+++ Cookbook_Web_App/Cookbook_Web_AppTDD/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Cookbook_Web_AppTDD
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- Cookbook_Web_App/Cookbook_Web_App_TDD/UnitTest1.cs
+++ Cookbook_Web_App/Cookbook_Web_App_TDD/UnitTest1.cs
@@ -1,13 +1,37 @@
 using System;
 using Xunit;
+using Cookbook_Web_App.Models;
 
 namespace Cookbook_Web_App_TDD
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanGetNameOfSavedRecipe()
         {
+            SavedRecipe savedRecipe = new SavedRecipe();
+            savedRecipe.Name = "ChickenParm";
+
+            Assert.Equal("ChickenParm", savedRecipe.Name);
+
+        }
+        [Fact]
+        public void CanSetNameOfSavedRecipe()
+        {
+            SavedRecipe savedRecipe = new SavedRecipe();
+            savedRecipe.Name = "ChickenParm";
+            savedRecipe.Name = "Chicken Alfredo";
+
+            Assert.Equal("Chicken Alfredo", savedRecipe.Name);
+
+        }
+        [Fact]
+        public void CanGetNameOfUser()
+        {
+            User user = new User();
+            user.User
+
+            Assert.Equal();
 
         }
     }
