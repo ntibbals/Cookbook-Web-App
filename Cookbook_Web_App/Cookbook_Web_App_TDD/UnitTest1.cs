968d227fbb61f81a8e0ddec6b6709288001c0e3a
Tulip Pink <pinktulips@hotmail.com>
2019-02-06 14:17:15 -0800

    user tests

--- Cookbook_Web_App/Cookbook_Web_App_TDD/UnitTest1.cs
+++ Cookbook_Web_App/Cookbook_Web_App_TDD/UnitTest1.cs
@@ -15,6 +15,19 @@ namespace Cookbook_Web_App_TDD
             Assert.Equal("ChickenParm", savedRecipe.Name);
 
         }
+       
+
+        [Fact]
+        public void CanSetNameOfSavedRecipe()
+        {
+            SavedRecipe savedRecipe = new SavedRecipe();
+            savedRecipe.Name = "ChickenParm";
+            savedRecipe.Name = "Chicken Alfredo";
+
+            Assert.Equal("Chicken Alfredo", savedRecipe.Name);
+
+        }
+
         [Fact]
         public void CanGetSavedRecipeID()
         {
@@ -36,16 +49,48 @@ namespace Cookbook_Web_App_TDD
 
         }
 
+
         [Fact]
-        public void CanSetNameOfSavedRecipe()
+        public void CanGetAPIReference()
         {
             SavedRecipe savedRecipe = new SavedRecipe();
-            savedRecipe.Name = "ChickenParm";
-            savedRecipe.Name = "Chicken Alfredo";
+            savedRecipe.APIReference = 1;
 
-            Assert.Equal("Chicken Alfredo", savedRecipe.Name);
+            Assert.Equal(1, savedRecipe.APIReference);
 
         }
+
+        [Fact]
+        public void CanSetAPIReference()
+        {
+            SavedRecipe savedRecipe = new SavedRecipe();
+            savedRecipe.APIReference = 1;
+            savedRecipe.APIReference = 2;
+
+            Assert.Equal(2, savedRecipe.APIReference);
+
+        }
+        [Fact]
+        public void CanGetUserID()
+        {
+            SavedRecipe savedRecipe = new SavedRecipe();
+            savedRecipe.UserID = 1;
+
+            Assert.Equal(1, savedRecipe.UserID);
+
+        }
+
+        [Fact]
+        public void CanSetUserID()
+        {
+            SavedRecipe savedRecipe = new SavedRecipe();
+            savedRecipe.UserID = 1;
+            savedRecipe.UserID = 2;
+
+            Assert.Equal(2, savedRecipe.UserID);
+
+        }
+
         [Fact]
         public void CanGetNameOfUser()
         {
