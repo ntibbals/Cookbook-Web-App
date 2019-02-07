f558d21944356dd7268cdbe34ed100af10819e05
Tulip Pink <pinktulips@hotmail.com>
2019-02-06 17:41:03 -0800

    saved recipe exists

--- Cookbook_Web_App/Cookbook_Web_App/Models/Services/SavedRecipeService.cs
+++ Cookbook_Web_App/Cookbook_Web_App/Models/Services/SavedRecipeService.cs
@@ -19,10 +19,16 @@ namespace Cookbook_Web_App.Models.Services
 
         //create
         public async Task DeleteSavedRecipe(int id)
-        { 
+        {
             SavedRecipe savedRecipe = _context.SavedRecipe.FirstOrDefault(s => s.SavedRecipeID == SavedRecipeID);
             _context.SavedRecipe.Remove(savedRecipe);
             await _context.SaveChangesAsync();
+        }
 
+        public bool SavedRecipeExists(int id)
+        {
+            return _context.SavedRecipe.Any(s => s.SavedRecipeID == id);
+
+        }
     }
 }
