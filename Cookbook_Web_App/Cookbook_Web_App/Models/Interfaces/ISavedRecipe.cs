dcd33afc1d717518485f3652b37bc7d5196563f6
Tulip Pink <pinktulips@hotmail.com>
2019-02-06 17:03:42 -0800

    saved recipe service

---
+++ Cookbook_Web_App/Cookbook_Web_App/Models/Interfaces/ISavedRecipe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook_Web_App.Models.Interfaces
+{
+    interface ISavedRecipe
+    {
+    }
+}
---
+++ Cookbook_Web_App/Cookbook_Web_App/Models/Services/SavedRecipeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook_Web_App.Models.Services
+{
+    public class SavedRecipeService : ISavedRecipe
+    {
+    }
+}
