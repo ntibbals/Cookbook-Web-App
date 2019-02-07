31c485219051fe25dfe8be5f267c1f4610d13eb0
Tulip Pink <pinktulips@hotmail.com>
2019-02-06 17:06:41 -0800

    create task

--- Cookbook_Web_App/Cookbook_Web_App/Models/Services/SavedRecipeService.cs
+++ Cookbook_Web_App/Cookbook_Web_App/Models/Services/SavedRecipeService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Cookbook_Web_App.Data;
+using Cookbook_Web_App.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,13 @@ namespace Cookbook_Web_App.Models.Services
 {
     public class SavedRecipeService : ISavedRecipe
     {
+        private CookbookDbContext _context { get; }
+
+        public SavedRecipeService(CookbookDbContext context)
+        {
+            _context = context;
+        }
+
+        //create
     }
 }
